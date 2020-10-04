bcdd147d703a37610a13d1f69b9b775e95437357
James Martin <37787591+jsmartin15@users.noreply.github.com>
2020-10-03 22:48:52 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from colorama import init, Fore, Back 
+import sys
+
+init() 
+
+response = requests.get(
+    'https://icanhazdadjoke.com/', 
+    headers={'Accept':'application/json'})
+
+print( Back.BLUE + Fore.YELLOW + 'Your dad joke: {0}'.format( response.json()['joke']))
+
+input("Press ENTER to continue!")
